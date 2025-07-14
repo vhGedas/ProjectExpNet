5265c734c4cf89b70e4ae506cb31ccb9a5025848
AUREO\concy <concyinfo@gmail.com>
2025-07-14 17:05:22 -0300

    Teste de conexao entre usuarios

--- ProjectExpNet/ProjectExpNet/Form1.Designer.cs
+++ ProjectExpNet/ProjectExpNet/Form1.Designer.cs
@@ -28,32 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
+            button1 = new Button();
             SuspendLayout();
             // 
-            // label1
+            // button1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(87, 184);
-            label1.Name = "label1";
-            label1.Size = new Size(38, 15);
-            label1.TabIndex = 0;
-            label1.Text = "label1";
+            button1.Location = new Point(325, 208);
+            button1.Name = "button1";
+            button1.Size = new Size(139, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Thouche here!";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(label1);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private Label label1;
+        private Button button1;
     }
 }
--- ProjectExpNet/ProjectExpNet/Form1.cs
+++ ProjectExpNet/ProjectExpNet/Form1.cs
@@ -5,6 +5,25 @@ namespace ProjectExpNet
         public Form1()
         {
             InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado;
+
+            do
+            {
+                resultado = MessageBox.Show("Are you gay?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (resultado == DialogResult.No)
+                {
+                    resultado = MessageBox.Show("I know... Confirm you are gay?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                }
+
+            } while (resultado != DialogResult.Yes);
+
+            MessageBox.Show("I knew it! ??", "Gotcha", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
